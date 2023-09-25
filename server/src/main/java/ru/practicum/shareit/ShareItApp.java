223fe48532f69aa8555a3bfd4ce8ed16a07ff660
Катя <ek8512@yandex.ru>
2023-09-25 21:47:38 +0300

    add docker

--- server/src/main/java/ru/practicum/shareit/ShareItApp.java
+++ server/src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -3,11 +3,15 @@ package ru.practicum.shareit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class ShareItApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShareItApp.class, args);
+		SpringApplication app = new SpringApplication(ShareItApp.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", "9090"));
+		app.run(args);
 	}
 
 }
